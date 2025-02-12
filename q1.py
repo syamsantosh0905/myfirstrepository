0cadacb31736490582540d0461c677f033d1bed3
Syam Santosh <syamsantosh092001@gmail.com>
2025-02-13 01:40:30 +0530

    Added a new Python code file

---
+++ q1.py
@@ -0,0 +1,9 @@
+print(abs(-3))
+print(pow(2,3))
+print(min(4,5))
+print(max(4,5))
+print(divmod(18,5))      # ramainder and quotient
+print(round(4.5455,2))    # rounds the given number upto specified points
+print(bin(254))
+print(hex(254))
+print(oct(254))
